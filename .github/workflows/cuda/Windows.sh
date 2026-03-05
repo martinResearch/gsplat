94fadbbd083abf3f728c6aad452a07cfea14ab22
Martin de La Gorce <madelago@microsoft.com>
2026-03-05 09:01:07 +0000

    fix: Windows builds - upgrade CUDA 12.8.1, rename Git link.exe, robustify CUDA_HOME

--- .github/workflows/cuda/Windows.sh
+++ .github/workflows/cuda/Windows.sh
@@ -10,8 +10,8 @@ curl -k -L "https://drive.google.com/u/0/uc?id=1injUyo3lnarMgWyRcXqKg4UGnN0ysmuq
 case ${1} in
   cu128)
     CUDA_SHORT=12.8
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
-    CUDA_FILE=cuda_${CUDA_SHORT}.0_570.86_windows.exe
+    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.1/local_installers
+    CUDA_FILE=cuda_${CUDA_SHORT}.1_572.61_windows.exe
     ;;
   cu126)
     CUDA_SHORT=12.6
